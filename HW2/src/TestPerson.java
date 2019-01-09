65c7fa940031dd95ccf22aa706f82777d6ce8d2b
gmarler20 <39546103+gmarler20@users.noreply.github.com>
2019-01-09 11:20:42 -0800

    Add files via upload

---
+++ HW2/src/TestPerson.java
@@ -0,0 +1,16 @@
+
+public class TestPerson 
+{
+	public static void main(String[] args) {
+
+		City C = new City();
+
+		C.OutputNames();
+		C.OutputBuildings();
+		C.OutputPolice();
+		C.OutputKidsandTeachers();
+		C.PayEmployeeSalary(4000);
+		C.OutputNames();
+	}
+
+}
